a439d5da6e1d9fcd7117bf4cd0c77cdad3ac08f0
dbeaudoinfortin <dbeaudoinfortin@thinkingphones.com>
2024-02-05 23:09:53 -0500

    Added multithreaded extraction of tar file.

--- src/GitUnbundle.java
+++ src/GitUnbundle.java
@@ -10,6 +10,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.LinkedBlockingDeque;
@@ -68,16 +70,16 @@ public class GitUnbundle {
 			
 			futures.add(THREAD_POOL.submit(new Runnable() {
 				 
-				 @Override
-		            public void run() {
-					 
-					 Process process = null;
-					 BufferedReader stdInput = null;
-					 BufferedReader stdError = null;
-					 
-					 int threadId = THREAD_ID_COUNTER.getAndIncrement();
-					 
-					 try {
+				@Override
+				public void run() {
+				 
+					Process process = null;
+					BufferedReader stdInput = null;
+					BufferedReader stdError = null;
+				 
+					int threadId = THREAD_ID_COUNTER.getAndIncrement();
+				 
+					try {
 						System.out.println(threadId + ":: Creating git repo in: " + unbundledDir);
 						process = Runtime.getRuntime().exec("git init", null, unbundledDir);
 						stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -91,13 +93,14 @@ public class GitUnbundle {
 							System.out.println(threadId + ":: Git repo created in: " + unbundledDir);
 							printErrorMessages(threadId, stdInput, stdError);
 						}
-					 } catch (Exception e) {
-						System.out.println(threadId + ":: ERROR during git init command: " + e.getMessage());
-						e.printStackTrace();
+					 } catch (Throwable t) {
+						System.out.println(threadId + ":: ERROR during git init command: " + t.getMessage());
+						t.printStackTrace();
+						return; //Don't throw a runtime exception, let the other threads run
 					 } finally {
 						if (null != process) process.destroy();
 					 }
-					 
+				 
 					 try {
 							
 						System.out.println(threadId + ":: Unbundling file: " + bundleFile);
@@ -116,9 +119,10 @@ public class GitUnbundle {
 							bundleFile.delete();
 						}
 						printErrorMessages(threadId, stdInput, stdError);
-					} catch (Exception e) {
-						System.out.println(threadId + ":: ERROR: " + e.getMessage());
-						e.printStackTrace();
+						
+					} catch (Throwable t) {
+						System.out.println(threadId + ":: ERROR during git unbundling: " + t.getMessage());
+						t.printStackTrace(); //Don't throw a runtime exception, let the other threads run
 					} finally {
 						if (null != process) process.destroy();
 					}
@@ -126,13 +130,7 @@ public class GitUnbundle {
 			}));
 		});
 		
-		futures.forEach(f->{
-			try {
-				f.get();
-			} catch (InterruptedException | ExecutionException e) {
-				e.printStackTrace();
-			}
-		});		
+		waitForTaskCompletion(futures);
 	}
 	
 	private static void printErrorMessages(int threadId, BufferedReader stdInput, BufferedReader stdError) throws IOException {
@@ -157,41 +155,33 @@ public class GitUnbundle {
 			int index = fileName.indexOf(".bundle");
 			if (index < 1) return;
 			
-			futures.add(THREAD_POOL.submit( new Runnable() {
+			futures.add(THREAD_POOL.submit(new Runnable() {
 				
 				@Override
 	            public void run() {
 					int threadId = THREAD_ID_COUNTER.getAndIncrement();
 					
-					File newDir = unbundledPath.resolve(bundleFile.getName().substring(0, index)).toFile();
-					if(newDir.exists()) {
-						try {
+					try {
+						File newDir = unbundledPath.resolve(bundleFile.getName().substring(0, index)).toFile();
+						if(newDir.exists()) {
 							if(FileUtils.isEmptyDirectory(newDir)) {
 								System.out.println(threadId + ":: Directory already exists and is empty: " + newDir);
 							} else {
 								System.out.println(threadId + ":: Directory already exists, deleting: " + newDir);
 								FileUtils.deleteDirectory(newDir);
 							}
-							
-						} catch (IOException e) {
-							System.out.println(threadId + ":: ERROR: " + e.getMessage());
-							e.printStackTrace();
 						}
+						System.out.println(threadId + ":: Creating dir: " + newDir);
+						newDir.mkdir();
+					} catch(Throwable t) {
+						System.out.println(threadId + ":: ERROR during directory creation: " + t.getMessage());
+						throw new RuntimeException(t); //Don't keep processing
 					}
-					System.out.println(threadId + ":: Creating dir: " + newDir);
-					newDir.mkdir();
 				}
 			}));
 		});
 		
-		futures.forEach(f->{
-			try {
-				f.get();
-			} catch (InterruptedException | ExecutionException e) {
-				e.printStackTrace();
-			}
-		});
-		
+		waitForTaskCompletion(futures);
 		System.out.println("All directories created.");
 	}
 	
@@ -216,35 +206,62 @@ public class GitUnbundle {
 	}
 	
 	private static void extractTarFile(Path rootPath, Path flattenedPath) throws IOException {
-		try (FileInputStream fis = new FileInputStream(rootPath.toString());
-				BufferedInputStream bis = new BufferedInputStream(fis, TAR_BUFFER_SIZE);
-				TarArchiveInputStream tarStream = new TarArchiveInputStream(fis)) {
-	
-			System.out.println("Reading tar file: " + rootPath);
-			TarArchiveEntry entry;
-		    while ((entry = tarStream.getNextEntry()) != null) {
-		    	System.out.println("Looking at: " + entry.getName());
-		    	if(entry.isFile() && entry.getName().endsWith(".bundle")) {
-		    		System.out.println("Matched bundle file: " + entry.getName());
-		    		
-		    		Path entryPath = Paths.get(entry.getName()).getFileName();
-		    		Path target = Paths.get(flattenedPath.toString(), entryPath.toString());
-		    		File targetFile = target.toFile();
-		    		
-		    		if (targetFile.exists()) {
-						if(!targetFile.isDirectory() && (targetFile.length() == entry.getRealSize())) {
-							System.out.println("Bundle file already exists, skipping: " + target);
-						} else {
-							System.out.println("Bundle file conflicts with existing file or directory: " + target);
-							throw new FileAlreadyExistsException(target.toString());
-						}
-		    		} else {
-		    			System.out.println("Copying bundle file "  + entry.getName() + " to path " + flattenedPath);
-			    		Files.copy(tarStream, target);
-		    		}
-		    	}
-		    }
+		List<Future<?>> futures = new ArrayList<Future<?>>();
+		
+		//Used for co-ordination between threads
+		Set<String> entrySet = ConcurrentHashMap.newKeySet(1000);
+		
+		for (int i = 0; i < THREAD_COUNT; i++) {
+			
+			futures.add(THREAD_POOL.submit(new Runnable() {
+				
+				@Override
+				public void run()  {
+					int threadId = THREAD_ID_COUNTER.getAndIncrement();
+					
+					//Open the tar file once per thread, this may lead to a lot of contention and memory usage
+					//if the number of concurrent threads is high
+					try (FileInputStream fis = new FileInputStream(rootPath.toString());
+							BufferedInputStream bis = new BufferedInputStream(fis, TAR_BUFFER_SIZE);
+							TarArchiveInputStream tarStream = new TarArchiveInputStream(fis)) {
+				
+						System.out.println("Reading tar file: " + rootPath);
+						TarArchiveEntry entry;
+					    while ((entry = tarStream.getNextEntry()) != null) {
+					    	
+					    	if(!entrySet.add(entry.getName())) continue; //Another thread took this file
+					    			
+					    	System.out.println("Looking at: " + entry.getName());
+					    	if(entry.isFile() && entry.getName().endsWith(".bundle")) {
+					    		System.out.println("Matched bundle file: " + entry.getName());
+					    		
+					    		//Found a match, now extract it from the tar file
+					    		Path entryPath = Paths.get(entry.getName()).getFileName();
+					    		Path target = Paths.get(flattenedPath.toString(), entryPath.toString());
+					    		File targetFile = target.toFile();
+					    		
+					    		if (targetFile.exists()) {
+									if(!targetFile.isDirectory() && (targetFile.length() == entry.getRealSize())) {
+										System.out.println("Bundle file already exists, skipping: " + target);
+									} else {
+										System.out.println("Bundle file conflicts with existing file or directory: " + target);
+										throw new FileAlreadyExistsException(target.toString());
+									}
+					    		} else {
+					    			System.out.println("Copying bundle file "  + entry.getName() + " to path " + flattenedPath);
+						    		Files.copy(tarStream, target);
+					    		}
+					    	}
+					    }
+					} catch(Throwable t) {
+						System.out.println(threadId + ":: ERROR tar file extraction: " + t.getMessage());
+						throw new RuntimeException(t); //Don't keep processing
+					}
+				}
+			}));
 		}
+		
+		waitForTaskCompletion(futures);
 	}
 	
 	private static void flattenDirStructure(Path rootPath, Path flattenedPath) throws IOException {
@@ -270,12 +287,25 @@ public class GitUnbundle {
 					System.out.println("Bundle file already exists, skipping: " + target);
 				} else {
 					System.out.println("Bundle file conflicts with existing file or directory: " + target);
-					throw new RuntimeException(e);
+					throw new RuntimeException(e); //Don't keep processing
 				}
 			} catch (IOException e) {
-				throw new RuntimeException(e);
+				throw new RuntimeException(e); //Don't keep processing
 			}
 		});
 	}
 
+	private static void waitForTaskCompletion(List<Future<?>> futures) {
+		futures.forEach(f->{
+			try {
+				f.get();
+			} catch (InterruptedException e) {
+				System.out.println("Failed to wait for completion of tasks.");
+				throw new RuntimeException(e); 
+			} catch (ExecutionException e) {
+				System.out.println("Task execution failed.");
+				throw new RuntimeException(e);
+			}
+		});
+	}
 }
